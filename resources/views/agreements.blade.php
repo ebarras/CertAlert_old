e5ec336d4b2849da742538e8298825fbebb98df0
ebarras <ebarras@gmail.com>
2017-12-27 15:54:46 -0600

    updating bootstrap and make agreement add button a demo modal

--- resources/views/agreements.blade.php
+++ resources/views/agreements.blade.php
@@ -9,7 +9,27 @@
 @section('content')
 <div class="container-fluid">
     <div class="row row-header-buttons justify-content-end">
-        <button type="button" class="btn btn-primary"><i class="fa fa-plus" aria-hidden="true"></i> Add Agreement</button>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal"><i class="fa fa-plus" aria-hidden="true"></i> Add Agreement</button>
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                ...
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
     <div class="row">
       <div class="table">
