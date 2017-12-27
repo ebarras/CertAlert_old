686007202f28fdbf5d1f55602f66787d64bff2e5
ebarras <ebarras@gmail.com>
2017-12-27 15:50:06 -0600

    adding add agreement button with fontawesome icon

--- resources/views/agreements.blade.php
+++ resources/views/agreements.blade.php
@@ -3,10 +3,14 @@
 @section('page-specific-css')
 <!-- Page Specific CSS -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css">
+    <script src="https://use.fontawesome.com/6120d7df05.js"></script>
 @endsection
 
 @section('content')
 <div class="container-fluid">
+    <div class="row row-header-buttons justify-content-end">
+        <button type="button" class="btn btn-primary"><i class="fa fa-plus" aria-hidden="true"></i> Add Agreement</button>
+    </div>
     <div class="row">
       <div class="table">
         <table id="home-agreements-table" class="table table-striped table-bordered" cellspacing="0" width="100%">
